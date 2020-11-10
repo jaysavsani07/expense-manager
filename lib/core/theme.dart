c868e84c3f30b36c533a8b4254007c317cd5319e
Mehul Makwana <mehulm@nividata.com>
2020-11-10 17:31:22 +0530

    change history page design

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -2,27 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData get theme {
-    final themeData = ThemeData.dark();
-    final textTheme = themeData.textTheme;
-    final body1 = textTheme.body1.copyWith(decorationColor: Colors.transparent);
-
-    return ThemeData.dark().copyWith(
-      primaryColor: Colors.grey[800],
-      accentColor: Colors.cyan[300],
-      buttonColor: Colors.grey[800],
-      textSelectionColor: Colors.cyan[100],
-      toggleableActiveColor: Colors.cyan[300],
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: Colors.cyan[300],
-      ),
-      snackBarTheme: SnackBarThemeData(
-        backgroundColor: themeData.dialogBackgroundColor,
-        contentTextStyle: body1,
-        actionTextColor: Colors.cyan[300],
-      ),
-      textTheme: textTheme.copyWith(
-        body1: body1,
-      ),
+    return ThemeData(
+      brightness: Brightness.light,
+      backgroundColor: Colors.white30,
+      accentColor: Colors.yellow[300],
     );
   }
 }
--- lib/ui/history/history.dart
+++ lib/ui/history/history.dart
@@ -3,6 +3,7 @@ import 'package:expense_manager/ui/app/app_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:intl/intl.dart';
 import 'package:redux/redux.dart';
 
 class History1 extends StatelessWidget {
@@ -17,66 +18,106 @@ class History1 extends StatelessWidget {
         return ListView(
           shrinkWrap: true,
           children: vm.list
-              .map((History history) => Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(history.title),
-                      ListView(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        children: history.list
-                            .map((e) => Container(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 5),
-                                  width: double.maxFinite,
-                                  height: 110,
-                                  child: Card(
-                                    elevation: 2,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(8)),
-                                    ),
+              .map((History history) => Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          history.title,
+                          style: TextTheme().caption,
+                        ),
+                        ListView(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: history.list
+                              .map((e) => Container(
+                                    padding: EdgeInsets.symmetric(vertical: 8),
+                                    height: 66,
                                     child: Row(
                                       children: [
                                         Container(
                                           decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(8),
-                                                bottomLeft: Radius.circular(8)),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(25)),
                                             color: e.category.iconColor,
                                           ),
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 8),
-                                          height: double.maxFinite,
+                                          height: 50,
+                                          width: 50,
                                           child: Icon(e.category.icon),
                                         ),
-                                        Flexible(
-                                          flex: 4,
-                                          child: Column(
-                                            children: [
-                                              Text(e.category.name),
-                                              Text(e.entry.amount.toString())
-                                            ],
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 8,
+                                                right: 8,
+                                                bottom: 4,
+                                                top: 4),
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: [
+                                                Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  children: [
+                                                    Text(
+                                                      e.category.name,
+                                                      style: TextStyle(
+                                                          fontSize: 18,
+                                                          fontWeight:
+                                                              FontWeight.bold),
+                                                    ),
+                                                    Text(
+                                                        e.entry.amount
+                                                            .toString(),
+                                                        style: TextStyle(
+                                                            fontSize: 18,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold))
+                                                  ],
+                                                ),
+                                                Row(
+                                                  children: [
+                                                    Text(
+                                                      DateFormat('d MMM')
+                                                          .format(e.entry
+                                                              .modifiedDate),
+                                                      style:
+                                                          TextTheme().caption,
+                                                    ),
+                                                    Padding(
+                                                      padding: const EdgeInsets
+                                                              .symmetric(
+                                                          horizontal: 4),
+                                                      child: Icon(
+                                                        Icons.circle,
+                                                        size: 8,
+                                                      ),
+                                                    ),
+                                                    Text(
+                                                      DateFormat.Hm().format(
+                                                          e.entry.modifiedDate),
+                                                      style:
+                                                          TextTheme().caption,
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
-                                        Flexible(
-                                          flex: 4,
-                                          child: Column(
-                                            children: [
-                                              Text(e.category.name),
-                                              Text(e.entry.modifiedDate
-                                                  .toString())
-                                            ],
-                                          ),
-                                        )
                                       ],
                                     ),
-                                  ),
-                                ))
-                            .toList(),
-                      )
-                    ],
+                                  ))
+                              .toList(),
+                        )
+                      ],
+                    ),
                   ))
               .toList(),
         );
