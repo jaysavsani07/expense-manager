ea9a7e8db79a0a100507bc67dc1f35756c1b3a13
Mehul Makwana <mehulm@nividata.com>
2021-02-11 12:19:00 +0530

    Show category pia chart to dashboard

--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:expense_manager/ui/dashboard/dashboard_state.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -25,55 +26,104 @@ class Dashboard extends ConsumerWidget {
                     Icon(Icons.settings_outlined).p16().onInkTap(() {})
                   ],
                 ),
-                "${NumberFormat.simpleCurrency().currencySymbol}${vm.list.map((e) => e.total).fold(0.0, (previousValue, element) => previousValue + element).toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
+                "${NumberFormat.simpleCurrency().currencySymbol}${vm.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
                     .text
                     .bold
                     .xl5
                     .make()
                     .pSymmetric(h: 16),
                 8.heightBox,
-                "${NumberFormat.simpleCurrency().currencySymbol}${vm.list.first.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")} Today"
+                "${NumberFormat.simpleCurrency().currencySymbol}${vm.today.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")} Today"
                     .text
                     .green500
                     .make()
                     .pOnly(left: 18),
                 16.heightBox,
-                ListView(
-                  shrinkWrap: true,
-                  children: vm.list
-                      .map((e) => Row(
-                            children: [
-                              Icon(
-                                e.category.icon,
-                                color: Vx.white,
-                              )
-                                  .box
-                                  .p12
-                                  .height(80)
-                                  .withDecoration(BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(8),
-                                        bottomLeft: Radius.circular(8)),
-                                    color: e.category.iconColor,
-                                  ))
-                                  .make(),
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    e.category.name.text.bold.base.make(),
-                                    "${NumberFormat.simpleCurrency().currencySymbol}${e.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
-                                        .text
-                                        .lg
-                                        .make()
-                                  ],
-                                ).pSymmetric(v: 4, h: 8),
-                              ),
-                            ],
-                          ).card.zero.withRounded(value: 8).white.p8.make())
-                      .toList(),
-                ),
+                PageView(
+                  scrollDirection: Axis.horizontal,
+                  controller: PageController(
+                    initialPage: 0,
+                  ),
+                  children: [
+                    Column(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 1,
+                          child: PieChart(
+                            PieChartData(
+                                pieTouchData: PieTouchData(
+                                    touchCallback: (pieTouchResponse) {
+                                  vm.onGraphItemTeach(pieTouchResponse);
+                                }),
+                                borderData: FlBorderData(
+                                  show: false,
+                                ),
+                                sectionsSpace: 0,
+                                centerSpaceRadius: 80,
+                                sections: vm.graphList),
+                          ),
+                        ),
+                        GridView(
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2, childAspectRatio: 5),
+                          scrollDirection: Axis.vertical,
+                          children: vm.list
+                              .map((list) => Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Icon(
+                                        Icons.circle,
+                                        size: 16,
+                                        color: list.category.iconColor,
+                                      ),
+                                      8.widthBox,
+                                      list.category.name.text.make()
+                                    ],
+                                  ).box.height(12).make())
+                              .toList(),
+                        ).pSymmetric(h: 16).expand()
+                      ],
+                    ),
+                    ListView(
+                      shrinkWrap: true,
+                      children: vm.list
+                          .map((e) => Row(
+                                children: [
+                                  Icon(
+                                    e.category.icon,
+                                    color: Vx.white,
+                                  )
+                                      .box
+                                      .p12
+                                      .height(80)
+                                      .withDecoration(BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(8),
+                                            bottomLeft: Radius.circular(8)),
+                                        color: e.category.iconColor,
+                                      ))
+                                      .make(),
+                                  Expanded(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        e.category.name.text.bold.base.make(),
+                                        "${NumberFormat.simpleCurrency().currencySymbol}${e.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
+                                            .text
+                                            .lg
+                                            .make()
+                                      ],
+                                    ).pSymmetric(v: 4, h: 8),
+                                  ),
+                                ],
+                              ).card.zero.withRounded(value: 8).white.p8.make())
+                          .toList(),
+                    ),
+                  ],
+                ).expand(),
               ],
             ),
           ),
--- lib/ui/dashboard/dashboard_state.dart
+++ lib/ui/dashboard/dashboard_state.dart
@@ -1,5 +1,7 @@
 import 'package:expense_manager/data/models/category_with_sum.dart';
 import 'package:expense_manager/data/repository/entry_repository_imp.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,11 +13,44 @@ class DashboardViewModel with ChangeNotifier {
   EntryRepositoryImp entryDataSourceImp;
 
   List<CategoryWithSum> list = [];
+  List<PieChartSectionData> graphList = [];
+  int touchedIndex = -1;
+  double total=0;
+  double today=0;
 
   DashboardViewModel({@required this.entryDataSourceImp}) {
     entryDataSourceImp.getAllEntryWithCategory().listen((event) {
       list = event;
+      total=list.map((e) => e.total).fold(0.0, (previousValue, element) => previousValue + element);
+      today=list.first.total;
+      graphList = showingSections(list);
       notifyListeners();
     });
+  }
+
+  void onGraphItemTeach(PieTouchResponse pieTouchResponse) {
+    if (pieTouchResponse.touchInput is FlLongPressEnd ||
+        pieTouchResponse.touchInput is FlPanEnd) {
+      touchedIndex = -1;
+    } else {
+      touchedIndex = pieTouchResponse.touchedSectionIndex;
+    }
+    graphList = showingSections(list);
+    notifyListeners();
+  }
+
+  List<PieChartSectionData> showingSections(List<CategoryWithSum> list) {
+    return list.asMap().entries.map((e) {
+      return PieChartSectionData(
+        color: e.value.category.iconColor,
+        value: e.value.total,
+        title: '${100*e.value.total~/total}%',
+        radius: e.key == touchedIndex ? 60 : 50,
+        titleStyle: TextStyle(
+            fontSize: e.key == touchedIndex ? 20 : 16,
+            fontWeight: FontWeight.bold,
+            color: const Color(0xffffffff)),
+      );
+    }).toList();
   }
 }
