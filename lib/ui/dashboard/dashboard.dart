dea2370d92390ddf96ea071c444ff332a413fc33
Mehul Makwana <mehulm@nividata.com>
2021-02-11 13:48:36 +0530

    Page indicator added

--- lib/ui/dashboard/dashboard.dart
+++ lib/ui/dashboard/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class Dashboard extends ConsumerWidget {
@@ -39,94 +40,134 @@ class Dashboard extends ConsumerWidget {
                     .make()
                     .pOnly(left: 18),
                 16.heightBox,
-                PageView(
-                  scrollDirection: Axis.horizontal,
-                  controller: PageController(
-                    initialPage: 0,
-                  ),
-                  children: [
-                    Column(
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: PieChart(
-                            PieChartData(
-                                pieTouchData: PieTouchData(
-                                    touchCallback: (pieTouchResponse) {
-                                  vm.onGraphItemTeach(pieTouchResponse);
-                                }),
-                                borderData: FlBorderData(
-                                  show: false,
-                                ),
-                                sectionsSpace: 0,
-                                centerSpaceRadius: 80,
-                                sections: vm.graphList),
-                          ),
-                        ),
-                        GridView(
-                          shrinkWrap: true,
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2, childAspectRatio: 5),
-                          scrollDirection: Axis.vertical,
-                          children: vm.list
-                              .map((list) => Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(
-                                        Icons.circle,
-                                        size: 16,
-                                        color: list.category.iconColor,
-                                      ),
-                                      8.widthBox,
-                                      list.category.name.text.make()
-                                    ],
-                                  ).box.height(12).make())
-                              .toList(),
-                        ).pSymmetric(h: 16).expand()
-                      ],
-                    ),
-                    ListView(
-                      shrinkWrap: true,
-                      children: vm.list
-                          .map((e) => Row(
-                                children: [
-                                  Icon(
-                                    e.category.icon,
-                                    color: Vx.white,
-                                  )
-                                      .box
-                                      .p12
-                                      .height(80)
-                                      .withDecoration(BoxDecoration(
-                                        borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(8),
-                                            bottomLeft: Radius.circular(8)),
-                                        color: e.category.iconColor,
-                                      ))
-                                      .make(),
-                                  Expanded(
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        e.category.name.text.bold.base.make(),
-                                        "${NumberFormat.simpleCurrency().currencySymbol}${e.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
-                                            .text
-                                            .lg
-                                            .make()
-                                      ],
-                                    ).pSymmetric(v: 4, h: 8),
-                                  ),
-                                ],
-                              ).card.zero.withRounded(value: 8).white.p8.make())
-                          .toList(),
-                    ),
-                  ],
-                ).expand(),
+                PageView1(
+                  vm: vm,
+                ),
               ],
             ),
           ),
         ));
+  }
+}
+
+class PageView1 extends StatefulWidget {
+  final DashboardViewModel vm;
+
+  PageView1({@required this.vm}) : super();
+
+  @override
+  _PageViewState createState() => _PageViewState();
+}
+
+class _PageViewState extends State<PageView1> {
+  PageController controller;
+
+  @override
+  void initState() {
+    controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SmoothPageIndicator(
+          controller: controller,
+          count: 2,
+          effect: JumpingDotEffect(
+            activeDotColor: Vx.black,
+            dotHeight: 8,
+            dotWidth: 8,
+          ),
+        ),
+        PageView(
+          scrollDirection: Axis.horizontal,
+          controller: controller,
+          children: [
+            Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: PieChart(
+                    PieChartData(
+                        pieTouchData:
+                            PieTouchData(touchCallback: (pieTouchResponse) {
+                          widget.vm.onGraphItemTeach(pieTouchResponse);
+                        }),
+                        borderData: FlBorderData(
+                          show: false,
+                        ),
+                        sectionsSpace: 8,
+                        centerSpaceRadius: 80,
+                        sections: widget.vm.graphList),
+                  ),
+                ),
+                GridView(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, childAspectRatio: 5),
+                  scrollDirection: Axis.vertical,
+                  children: widget.vm.list
+                      .map((list) => Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.circle,
+                                size: 16,
+                                color: list.category.iconColor,
+                              ),
+                              8.widthBox,
+                              list.category.name.text.make()
+                            ],
+                          ).box.height(12).make())
+                      .toList(),
+                ).pSymmetric(h: 16).expand()
+              ],
+            ),
+            ListView(
+              shrinkWrap: true,
+              children: widget.vm.list
+                  .map((e) => Row(
+                        children: [
+                          Icon(
+                            e.category.icon,
+                            color: Vx.white,
+                          )
+                              .box
+                              .p12
+                              .height(80)
+                              .withDecoration(BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    bottomLeft: Radius.circular(8)),
+                                color: e.category.iconColor,
+                              ))
+                              .make(),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                e.category.name.text.bold.base.make(),
+                                "${NumberFormat.simpleCurrency().currencySymbol}${e.total.toString().replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")}"
+                                    .text
+                                    .lg
+                                    .make()
+                              ],
+                            ).pSymmetric(v: 4, h: 8),
+                          ),
+                        ],
+                      ).card.zero.withRounded(value: 8).white.p8.make())
+                  .toList(),
+            ),
+          ],
+        ).expand()
+      ],
+    ).expand();
   }
 }
