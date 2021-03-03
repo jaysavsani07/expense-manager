61e41f5467446aa79f8cdf2b9feef62119a2586b
Jay Savsani <jay@nividata.com>
2021-03-03 22:55:16 +0530

    echo working directiong

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -4,7 +4,7 @@
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
 --output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && cd -
-echo ./
+echo $PWD
 FILE=./expensemanager.jks
 if test -f "$FILE"; then
     echo "$FILE exists."
